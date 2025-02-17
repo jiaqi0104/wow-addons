eed5333eb05565a6bfe9bb0d416f60d6b6e0b881
jiaqi0104 <zxx13889883242@gmail.com>
2025-02-17 19:13:14 +0800

    Add files via upload

---
+++ Fonts/新建文本文档.bat
@@ -0,0 +1,11 @@
+@echo off
+@echo 生成
+Copy 1.ttf Arial.ttf
+copy 1.ttf BigNoodleTitling.ttf
+copy 1.ttf DieDieDie.ttf
+copy 1.ttf Expressway.ttf
+copy 1.ttf Invisible.ttf
+copy 1.ttf PtSansNarrow.ttf
+copy 1.ttf Visitor.ttf
+@echo 完成
+pause
